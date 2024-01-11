5727ccc950c6cbe648c8e0474e53349cc8eaf41a
Petar Jovanovic <pecazajecar@gmail.com>
2024-01-11 21:20:05 +0100

    Initial commit

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: 'Username',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: '1', name: 'Peca', password: 'Nextauth' };
+
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
